464110dfeec0276dba8bcaab934e601c6cbd7a7d
Lachee <lachlan.h@hotmail.com>
2020-05-28 18:16:20 +1000

    Moved server to its own git

--- src/mixy-wss/components/Component.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Component {
-    static allowedComponents = {
-        'Constellation': './components/ConstellationComponent.js',
-        'Interactive':   './components/InterativeComponent.js',
-        'R6Siege':      './components/R6SiegeComponent.js',
-    };
-}
